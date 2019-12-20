3f0979c886a340577e57ae4ef20893aa37b592b6
Wendy Garcia <wendy.fern84@gmail.com>
2019-12-20 15:44:02 -0800

    add starter Random.js file

---
+++ src/Components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Random} from "../Random/Random";
+
+class App extends React.Component {
+    render() {
+        return <Random />
+    }
+}
+
+export default App;
---
+++ src/Components/Random/Random.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export class Random extends React.Component {
+    componentDidMount() {
+        this.applyColor();
+      }
+    
+      componentDidUpdate(prevProps, prevState) {
+        this.applyColor();
+      }
+    
+      formatColor(ary) {
+        return 'rgb(' + ary.join(', ') + ')';
+      }
+    
+      isLight() {
+        const rgb = this.state.color;
+        return rgb.reduce((a,b) => a+b) < 127 * 3;
+      }
+    
+      applyColor() {
+        const color = this.formatColor(this.state.color);
+        document.body.style.background = color;
+      }
+    
+      chooseColor() {
+        const random = [];
+        for (let i = 0; i < 3; i++) {
+          random.push(Math.floor(Math.random()*256));
+        }
+        return random;
+      }
+    
+      render() {
+        return (
+          <div>
+            <h1 className={this.isLight() ? 'white' : 'black'}>
+    
+            </h1>
+          </div>
+        );
+      }
+}
